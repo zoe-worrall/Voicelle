8a32882cb4a6bb3fb9ab2763893a03d41adbe1c0
ConnorBoi <67206120+ConnorBoi@users.noreply.github.com>
2020-07-17 12:54:25 -0600

    Update audio - Copy.py

--- audio - Copy.py
+++ audio - Copy.py
@@ -3,61 +3,80 @@ import subprocess
 import ffmpeg
 import wave
 import contextlib
+
+# importing librarys
+from speech_recognition import AudioData
+import speech_recognition as sr
+import subprocess
+import ffmpeg
+import wave
+import contextlib
+
+check = False
+audio = ""
 r = sr.Recognizer()
-r.energy_threshold=2000
-r.pause_threshold=1
+r.energy_threshold = 2000
+r.pause_threshold = 1
 mic = sr.Microphone(device_index=0)
+# Configuring the record function as well as a barrier for the mic to read(energy threshold)
+# Pause threshold will govern the length of silence needed to begin another line
+# ( Device index line governs the microphone that is used 0 is default
 
 with mic as source:
     print("file or mic? say which you want")
-    audio = r.listen(mic)
+    response = r.listen(mic)
+    # mic as source intiilises the microphone and audio detects speech from mic
 
-    try:
-        response = (r.recognize_google(audio))
-    except:
-        file = input("file name")
-        try:
-            print(r.recognize_google(file))
-        except:
-            command = "ffmpeg -i " + file + " -ab 160k -ac 2 -ar 44100 -vn audio.wav"
-            print(r.recognize_google(audio.wav))
-    print (response)
+    print(response)
 if response.lower == "mike\n":
+    # if said mic begin microphone program
     while response == response:
+        response = "yes"
+        while response.lower() == "yes":
+            print("begin when ready, if you stop speaking for 1 second, the block will end")
 
-        print("begin when ready, if you stop speaking for 1 second, the block will end")
-        while response == "yes":
-
-            audio = r.listen(mic) + "\n"
-            response = input(audio +" line 1" +"\nRecord second line?")
+        audio = r.listen(mic) + "\n"
+        response = input(audio + " line 1" + "\nRecord second line?")
 
 
 else:
 
-
     file = input("file name")
+    stream = ffmpeg.input(file)
+    audioia = stream.audio
+    out = ffmpeg.output(audioia, 'BIGFILEENERGY.flac')
+
+    # noinspection PyBroadException
     try:
-        print(r.recognize_google(file))
+
+        # attempt to mount and read file
+        file2 = r.record(out)
+        BIG_MEME = (r.recognize_google(file2))
+        print(BIG_MEME)
     except:
-       command = "ffmpeg -i "+file+ " -ab 160k -ac 2 -ar 44100 -vn audio.wav"
-       audio2 = r.record('audio.wav')
-       print(r.recognize_google(audio2))
+        print("U did a bad.")
 
-    with contextlib.closing(wave.open(audio,'r')) as f:
-        frames = f.getnframes()
-        rate = f.getframerate()
-        duration = frames/float(rate)
-        x=0
-        check == False
+    with contextlib.closing(wave.open(out, 'r')) as f:
+        # prototype rip audio from file
+        frames = file2.getnframes()
+        rate = file2.getframerate()
+        duration = frames / float(rate)
+        x = 0
+        check = False
+        text = ""
+        text_hold = ""
+
     while x < duration and check == True:
-        y=1
+        y = 1
 
-        text_temp = (r.recognize_google(audio,offset=x, duration=y))
+        text_temp = (r.recognize_google(file2, x, y))
         text = text + " " + text_temp
-        print (text_temp + " second 1")
-        x=x+y
-        if text== r.recognize_google(audio):
+        text_hold = text_hold + (text_temp + " second " + x)
+        x = x + y
+        # this code ticks up a second counter to read each x seconds of a file and compile and compare to the entire
+        # file then the code will if not correct check every 2 seconds of a file and so on and so forth
+        if text == r.recognize_google(audio):
             check = True
         elif x >= duration:
-            y = y+1
-            x=0
+            y = y + 1
+            x = 0
